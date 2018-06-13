e8a2c4d9f7223e698e463762fcf0f977d62d3642
Ryan Johnston <ryjo1026@gmail.com>
2018-06-13 17:36:35 +0200

    Added ManageSearchBar

--- src/components/Manage/ManageContainer.jsx
+++ src/components/Manage/ManageContainer.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
-import TitleBar from '../Common/TitleBar.jsx';
+import Grid from '@material-ui/core/Grid'
 
+import TitleBar from '../Common/TitleBar.jsx';
+import ManageSearchBar from './ManageSearchBar.jsx';
 
 class ManageContiner extends React.Component {
+  handleSearchClicked(event) {
+    console.log('click');
+  }
+
   render() {
     return (
       <div className='ManageContiner'>
-        <TitleBar title='Manage your IoT Device'/>
+        <TitleBar title='Manage Your IoT Device'/>
+        <Grid container spacing={40}>
+          <Grid item xs={1} sm={3}></Grid>
+          <Grid item xs={10} sm={6}>
+            <ManageSearchBar onSearchClicked={this.handleSearchClicked}/>
+          </Grid>
+          <Grid item xs={1} sm={3}></Grid>
+        </Grid>
       </div>);
   }
 }
---
+++ src/components/Manage/ManageSearchBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+// A Search bar and submit button to get devices from the contract by their name
+export default function ManageSearchBar({onSearchClicked}) {
+  return (<div className='ManageSearchBar' style={{display: 'flex', alignItems: 'center'}}>
+    <TextField
+      id="full-width"
+      label="Device Name"
+      helperText="Enter the name of the device you would like to manage"
+      margin="normal"
+      style={{width: '70%', marginRight: '10%'}}
+    />
+    <Button variant="raised"
+      color="primary"
+      style={{textTransform: 'none',
+      width: '10%',
+      height: '50%',
+      float: 'right',
+      verticalAlign: 'middle',
+      }}
+      onClick={onSearchClicked}>
+      Search
+    </Button>
+  </div>);
+}
+ManageSearchBar.propTypes = {
+  onSearchClicked: PropTypes.func.isRequired,
+};
