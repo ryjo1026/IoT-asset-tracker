26bc484791446bd1db746b52abe57ee4983f9fc7
Ryan Johnston <ryjo1026@gmail.com>
2018-06-13 16:54:09 +0200

    Initial setup

---
+++ src/components/Manage/ManageContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import TitleBar from '../Common/TitleBar.jsx';
+
+
+class ManageContiner extends React.Component {
+  render() {
+    return (
+      <div className='ManageContiner'>
+        <TitleBar title='Manage your IoT Device'/>
+      </div>);
+  }
+}
+
+export default ManageContiner;
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 
 // Import all routed components
 import Home from './Home/Home.jsx';
+import Manage from './Manage/ManageContainer.jsx';
 import Register from './Register/RegisterContainer.jsx';
 import Search from './Search/SearchContainer.jsx';
 
@@ -10,6 +11,7 @@ const Router = () => (
   <Switch>
     <Route exact path='/' component={Home}/>
     <Route exact path='/home' component={Home}/>
+    <Route exact path='/manage' component={Manage}/>
     <Route exact path='/register' component={Register}/>
     <Route exact path='/search' component={Search}/>
     {/* TODO <Route component={404}/> */}
